9f179d47542894eb9b87a69c90901044506b1159
dev-adeel-arshad <adeelarshad.dev@gmail.com>
2026-09-10 11:11:18 +0500

    update:username validation Updated

--- front_end/src/validator/userDataValidator.js
+++ front_end/src/validator/userDataValidator.js
@@ -2,11 +2,11 @@ import {z} from "zod";
 // VALIDATION FOR REGISTER
 const registerSchema = z.object({
     username:z.string()
-    .min(8,"Username must be at least 8 characters"),
+    .min(4,"Username must be at least 4 characters"),
     email:z.string()
     .email(),
     password:z.string()
-    .min(8,"Password must be at least 8 characters oncluding upperCase,lowerCase and Number!")
+    .min(8,"Password must be at least 8 characters including upperCase,lowerCase and Number!")
     .regex(/[A-Z]/)
     .regex(/[a-z]/)
     .regex(/[0-9]/),
@@ -16,7 +16,7 @@ const registerSchema = z.object({
 // VALIDATION FOR LOOGIN
  const loginSchema = z.object({
     email:z.string().email(),
-    password:z.string().min(8,"Password must be at least 8 characters oncluding upperCase,lowerCase and Number!").regex(/[A-Z]/).regex(/[a-z]/).regex(/[0-9]/)
+    password:z.string().min(8,"Password must be at least 8 characters including upperCase,lowerCase and Number!").regex(/[A-Z]/).regex(/[a-z]/).regex(/[0-9]/)
   })
 
 export { registerSchema ,loginSchema }
